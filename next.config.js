8a9bca56b5981a256670fcf39de9b0a1d14c54cb
Karan Chaurasiya <karanchaurasiya.012@gmail.com>
2025-07-29 21:56:29 +0530

    inlast

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['json-backend-8zn4.onrender.com', 'img.icons8.com'],
+    domains: [
+      "json-backend-8zn4.onrender.com", // doctor profile images
+      "img.icons8.com",                 // any icons
+      "randomuser.me",                 // user profile image
+    ],
   },
 };
 
